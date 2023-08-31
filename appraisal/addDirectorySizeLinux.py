d1d6cd25472f8d44cc629433eb6b19c1bbe1039c
Brock Stuessi <bstuessi@mikekelleyfoundation.org>
2023-08-31 16:52:25 -0700

    split directory size script for both os

--- appraisal/addDirectorySizeLinux.py
+++ appraisal/addDirectorySizeLinux.py
@@ -5,22 +5,17 @@ import subprocess
 # function that calls shell function du to calculate the size of a directory
 
 def du(path):
-    # Run the du command and capture its output as bytes
-    result_bytes = subprocess.check_output(['du', '-sAk', path])
-
-    # Convert the bytes to a string and split to get the size
-    result_string = result_bytes.decode('utf-8')
-    size_in_kilobytes = int(result_string.split()[0])
-
-    # Convert kilobytes to bytes (1 kilobyte = 1024 bytes)
-    size_in_bytes = size_in_kilobytes * 1024
-
-    return size_in_bytes
+    try:
+        size = subprocess.check_output(['sudo', 'du','-sb', '--apparent-size', path]).split()[0].decode('utf-8')
+        return size
+    except subprocess.CalledProcessError:
+        print(f"Issue retrieving directory size for {path}")  
+    
 
 
 def addDirectorySize(input_csv_path):
     with open(input_csv_path, 'r') as in_csvfile, \
-        open (f"{input_csv_path.split('.')[0]}_updated.csv", 'w') as out_csvfile:
+        open (f"{input_csv_path}_updated", 'w') as out_csvfile:
         reader = csv.DictReader(in_csvfile)
         writer = csv.DictWriter(out_csvfile, fieldnames=reader.fieldnames)
         writer.writeheader()
@@ -33,6 +28,6 @@ def addDirectorySize(input_csv_path):
 
 
 if __name__ == '__main__':
-    csv_path = '/Users/mkf26/Desktop/appraisal_workflow_testing/test.csv'
+    csv_path = '/home/bcadmin/Desktop/D_0214.csv'
     addDirectorySize(csv_path)
 
