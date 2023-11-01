84892ecd727439d8fc80610ee63cc14156448043
Brock Stuessi <bstuessi@mikekelleyfoundation.org>
2023-11-01 14:59:13 -0700

    update backup script to work on linux

--- appraisal/backupDB.sh
+++ appraisal/backupDB.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 timestamp=$(date "+%Y-%m-%dT%H_%M")
-db=digital_appraisal
-pg_dump -Fc $db > /Users/mkf26/Desktop/appraisal_db_backups/${db}_${timestamp}.dump
+db=digital_appraisal_new
+pg_dump -Fc $db > $HOME/Desktop/appraisal_db_backups/${db}_${timestamp}.dump
